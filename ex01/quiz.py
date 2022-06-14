27482bbfb6d9a6dbed923d5f9800c408d7bd0bd2
Sato060 <c0a2106090@edu.teu.ac.jp>
2022-06-14 15:42:11 +0900

    練習問題

---
+++ ex01/quiz.py
@@ -0,0 +1,47 @@
+import random
+import time
+def shutudai1():
+    d={}
+    d["Q1"]="答え1"
+    d["Q2"]="答え2"
+    quiz=random.choice(list(d.items()))
+    ans=kaito(quiz)
+    if quiz[1]==ans:
+        print("正解")
+    else:
+        print("不正解")
+
+def shutudai2():
+    d={}
+    d["Q1"]=["答え1","こたえ1","1"]
+    d["Q2"]=["答え2","こたえ2","2"]
+    quiz=random.choice(list(d.items()))
+    ans=kaito(quiz)
+    if ans in quiz[1]:
+        print("正解")
+    else:
+        print("不正解")
+
+def shutudai():
+    d={}
+    d["サザエの旦那の名前は？"]=["マスオ","ますお"]
+    d["カツオの妹の名前は？"]=["ワカメ","わかめ"]
+    d["タラオはカツオから見てどんな関係？"]=["甥","おい","甥っ子","おいっこ"]
+    quiz=random.choice(list(d.items()))
+    ans=kaito(quiz)
+    if ans in quiz[1]:
+        print("正解！！！")
+    else:
+        print("出直してこい")
+
+def kaito(a):
+    print(a[0])
+    start=time.time()
+    word=input("回答:")
+    end=time.time()
+    anstime=round(end-start,1)
+    print(f"経過時間:{anstime}秒")
+    return word
+
+if __name__ == "__main__":
+    shutudai()
