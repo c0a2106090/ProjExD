d7cbcee46581c11161c58b059750155d98abd8e4
Sato060 <c0a2106090@edu.teu.ac.jp>
2022-06-21 14:51:38 +0900

    第6

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,14 +4,14 @@ def Bottonmake(a):
     global entry
     entry=tk.Entry(root,justify="right",width=10,font=("Times New Roman", 40))
     entry.grid(columnspan=4)
-    for i in range(3):
-        for j in range(3):
+    for i in range(len(a)):
+        for j in range(len(a[0])):
             button=tk.Button(root,text=str(a[i][j]),height=2,width=4,font=("Times New Roman",30))
-            button.grid(row=i+1,column=j)
+            button.grid(row=i+1,column=j+1)
             button.bind("<1>",button_click)
-    button=tk.Button(root,text="0",height=2,width=4,font=("Times New Roman",30))
-    button.grid(row=4,column=0)
-    button.bind("<1>",button_click)
+    #button=tk.Button(root,text="0",height=2,width=4,font=("Times New Roman",30))
+    #button.grid(row=4,column=0)
+    #button.bind("<1>",button_click)
 
 def button_click(event):
     btn=event.widget
@@ -22,7 +22,7 @@ def button_click(event):
 if __name__ == "__main__":  
     root=tk.Tk()
     root.geometry("300x600")
-    Bottonname=[[9,8,7],[6,5,4],[3,2,1],[0,"",""]]
+    Bottonname=[[9,8,7],[6,5,4],[3,2,1],[0,"+","="]]
     Bottonmake(Bottonname)
 
     root.mainloop()
