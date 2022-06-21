d925e485b89a6e77f2750e048812a7975ca0f20c
Sato060 <c0a2106090@edu.teu.ac.jp>
2022-06-21 14:31:03 +0900

    第3

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,8 +5,15 @@ def Bottonmake(a):
         for j in range(3):
             button=tk.Button(root,text=str(a[i][j]),height=2,width=4,font=("Times New Roman",30))
             button.grid(row=i,column=j)
+            button.bind("<1>",button_click)
     button=tk.Button(root,text="0",height=2,width=4,font=("Times New Roman",30))
     button.grid(row=4,column=0)
+    button.bind("<1>",button_click)
+
+def button_click(event):
+    btn=event.widget
+    txt=btn["text"]
+    tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
 
 if __name__ == "__main__":  
     root=tk.Tk()
