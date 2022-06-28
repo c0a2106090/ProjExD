5de277758d8b7673e2af59bf070c52c7f952bcaf
Sato060 <c0a2106090@edu.teu.ac.jp>
2022-06-28 14:26:33 +0900

    第三

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,12 +1,19 @@
 from hashlib import blake2b
+from itertools import cycle
 import tkinter as tk
 import tkinter.messagebox as tkm
+
 
 if __name__ == "__main__":   
     root=tk.Tk()
     root.title("迷えるこうかとん")
-
     canvas=tk.Canvas(root,width=1500,height=900,bg="black")
     canvas.place(x=0,y=0)
     canvas.pack()
+    
+    tori=tk.PhotoImage(file="C:/Users/admin/Downloads/プロジェクト演習/ProjExD2022/ex03/fig/8.png")
+    cx=300
+    cy=400
+    canvas.create_image(cx,cy,image=tori,tag="tori")
+    
     tk.mainloop()
