467bc272dbab90a1625355224891aa7ad211925c
Sato060 <c0a2106090@edu.teu.ac.jp>
2022-06-28 16:53:04 +0900

    メイズ

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,12 +1,12 @@
 from hashlib import blake2b
-from itertools import cycle
-#from termios import CSIZE
+from itertools import count, cycle
 import tkinter as tk
 import tkinter.messagebox as tkm
 import maze_maker
 
+
 def key_down(event):
-    global key
+    global key 
     key=event.keysym
 
 def key_up(event):
@@ -15,14 +15,26 @@ def key_up(event):
 
 def main_proc():
     global mx,my,key
-    if key=="Up" and mass[my-1][mx]==0:
-        my-=1
-    elif key=="Down" and mass[my+1][mx]==0:
-        my+=1
-    elif key=="Left" and mass[my][mx-1]==0:
-        mx-=1
-    elif key=="Right" and mass[my][mx+1]==0:
-        mx+=1
+    if key=="Up" :
+        if mass[my-1][mx]==0:
+            my-=1
+        else:
+            root.destroy()
+    elif key=="Down" :
+        if mass[my+1][mx]==0:
+            my+=1
+        else:
+            root.destroy()
+    elif key=="Left" :
+        if mass[my][mx-1]==0:
+            mx-=1
+        else:
+            root.destroy()
+    elif key=="Right" :
+        if mass[my][mx+1]==0:
+            mx+=1
+        else:
+            root.destroy()
     x=mx*100+50
     y=my*100+50
     canvas.coords("tori",x,y)
@@ -31,12 +43,14 @@ def main_proc():
 if __name__ == "__main__":   
     root=tk.Tk()
     root.title("迷えるこうかとん")
+    label=tk.Label(root,font=(80))
     canvas=tk.Canvas(root,width=1500,height=900,bg="black")
     canvas.place(x=0,y=0)
     canvas.pack()
     
     mass=maze_maker.make_maze(15,9)
     maze_maker.show_maze(canvas,mass)
+
 
     tori=tk.PhotoImage(file="C:/Users/admin/Downloads/プロジェクト演習/ProjExD2022/ex03/fig/8.png")
     mx,my=1,1
