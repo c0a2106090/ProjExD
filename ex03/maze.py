74a4dea1a409afdedd7108a6c96a3b544f5d13c9
Sato060 <c0a2106090@edu.teu.ac.jp>
2022-06-28 13:29:31 +0900

    第二

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,7 +1,12 @@
+from hashlib import blake2b
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-if __name__ == "__main__":  
+if __name__ == "__main__":   
     root=tk.Tk()
-    root.title("迷えるこうかとん")  
+    root.title("迷えるこうかとん")
+
+    canvas=tk.Canvas(root,width=1500,height=900,bg="black")
+    canvas.place(x=0,y=0)
+    canvas.pack()
     tk.mainloop()
