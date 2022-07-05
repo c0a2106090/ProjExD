ed3d50a5bd2d8648587e63a8d3cc581152ccd372
Sato060 <c0a2106090@edu.teu.ac.jp>
2022-07-05 13:15:36 +0900

    第三

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -9,8 +9,13 @@ def main():
     clock=pg.time.Clock()
     clock.tick(1000)
 
+    tori_img=pg.image.load("C:/Users/admin/Downloads/プロジェクト演習/ProjExD2022/ex03/fig/8.png")
+    tori_img=pg.transform.rotozoom(tori_img,0,2.0)
+    tori_rect=tori_img.get_rect()
+
     while True:
         screen.blit(bg,rect_bg)
+        screen.blit(tori_img,tori_rect)
         pg.display.update() 
 
         for event in pg.event.get():#イベントキューからキーボードやマウスの動きを取得
