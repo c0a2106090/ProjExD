b2672db3e1cfd52371910109bb1209c10ba344d0
Sato060 <c0a2106090@edu.teu.ac.jp>
2022-07-05 13:15:04 +0900

    第二

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,16 +1,24 @@
 import pygame as pg
 import sys
-
+       
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen=pg.display.set_mode((1600,900))
     bg=pg.image.load("C:/Users/admin/Downloads/プロジェクト演習/ProjExD2022/ex04/pg_bg.jpg")
     rect_bg=bg.get_rect()
     clock=pg.time.Clock()
-    clock.tick(0.5)
+    clock.tick(1000)
+
+    while True:
+        screen.blit(bg,rect_bg)
+        pg.display.update() 
+
+        for event in pg.event.get():#イベントキューからキーボードやマウスの動きを取得
+            if event.type == pg.QUIT:      # 閉じるボタンが押されたら終了
+                return
 
 if __name__ == "__main__":
-    pg.init()
+    pg.init() 
     main()
-    pg.quit()
+    pg.quit()           # Pygameの終了(ないと終われない)
     sys.exit()
