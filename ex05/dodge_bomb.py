469386624e88780bc22df48b9445694835b98209
Sato060 <c0a2106090@edu.teu.ac.jp>
2022-07-12 14:40:29 +0900

    3限基本機能実装完了

--- ex05/dodge_bomb.py
+++ ex05/dodge_bomb.py
@@ -8,14 +8,14 @@ class Screen:
         pg.display.set_caption(title)
         self.sfc = pg.display.set_mode(size) # Surface
         self.rct = self.sfc.get_rect() 
-        self.bgi_sfc = pg.image.load(file)    # Surface
-        self.bgi_rct = self.bgi_sfc.get_rect()              # Rect
+        self.bgi_sfc = pg.image.load(file) # Surface
+        self.bgi_rct = self.bgi_sfc.get_rect() # Rect
     def blit(self):
         self.sfc.blit(self.bgi_sfc, self.bgi_rct)
 
 class Bird:
 #    key_delta=
-    def __init__(self,file,bairitsu,xy):
+    def __init__(self,file,bairitsu,xy): # xy:初期座標
         self.sfc=pg.image.load(file)
         self.sfc=pg.transform.rotozoom(self.sfc, 0, float(bairitsu))
         self.rct=self.sfc.get_rect()
@@ -41,48 +41,28 @@ class Bird:
                 self.rct.centerx += 1
             if key_states[pg.K_RIGHT]: 
                 self.rct.centerx -= 1
-        
+        self.blit(SCobj)
 
-def main():
-    clock = pg.time.Clock()
-    screen=Screen("逃げろ！こうかとん",(1600, 900),"ex04/pg_bg.jpg")
-    bird=Bird("ex03/fig/8.png",2.0,(900,400))
+class Bomb:
+    def __init__(self,color,r,v,SCobj): # v:x方向速度とy方向速度のタプル
+        self.sfc=pg.Surface((20, 20))
+        self.sfc.set_colorkey((0,0,0))
+        pg.draw.circle(self.sfc, color, (10, 10), r)
+        self.rct=self.sfc.get_rect()
+        self.rct.centerx = random.randint(0, SCobj.rct.width)
+        self.rct.centery = random.randint(0, SCobj.rct.height)
+        self.vx=+int(v[0])
+        self.vy=+int(v[1])
+    def blit(self,SCobj):
+        SCobj.sfc.blit(self.sfc, self.rct)
+    def update(self,SCobj):
+        self.rct.move_ip(self.vx, self.vy)
+        # 外枠に触れると-1を返す(yokoかtate)
+        yoko, tate = check_bound(self.rct, SCobj.rct)
+        self.vx *= yoko
+        self.vy *= tate
+        self.blit(SCobj)
 
-    # 練習5：爆弾
-    bmimg_sfc = pg.Surface((20, 20)) # Surface
-    bmimg_sfc.set_colorkey((0, 0, 0)) 
-    pg.draw.circle(bmimg_sfc, (255, 0, 0), (10, 10), 10)
-    bmimg_rct = bmimg_sfc.get_rect() # Rect
-    bmimg_rct.centerx = random.randint(0, screen.rct.width)
-    bmimg_rct.centery = random.randint(0, screen.rct.height)
-    vx, vy = +1, +1 # 練習6
-
-    while True:
-        screen.blit()
-
-        # 練習2
-        for event in pg.event.get():
-            if event.type == pg.QUIT: return
-        bird.blit(screen)
-        bird.update(screen)
-
-        # 練習6
-        bmimg_rct.move_ip(vx, vy)
-        # 練習5
-        screen.sfc.blit(bmimg_sfc, bmimg_rct)
-        # 練習7
-        yoko, tate = check_bound(bmimg_rct, screen.rct)
-        vx *= yoko
-        vy *= tate
-
-        # 練習8
-        if bird.rct.colliderect(bmimg_rct): return 
-
-        pg.display.update()
-        clock.tick(1000)
-
-
-# 練習7
 def check_bound(rct, scr_rct):
     '''
     [1] rct: こうかとん or 爆弾のRect
@@ -92,6 +72,22 @@ def check_bound(rct, scr_rct):
     if rct.left < scr_rct.left or scr_rct.right  < rct.right : yoko = -1 # 領域外
     if rct.top  < scr_rct.top  or scr_rct.bottom < rct.bottom: tate = -1 # 領域外
     return yoko, tate
+
+def main():
+    clock = pg.time.Clock()
+    screen=Screen("逃げろ！こうかとん",(1600, 900),"ex04/pg_bg.jpg")
+    bird=Bird("ex03/fig/8.png",2.0,(900,400))
+    bomb=Bomb((255,0,0),10,(1,1),screen)
+
+    while True:
+        screen.blit()
+        bird.update(screen)
+        bomb.update(screen)
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        if bird.rct.colliderect(bomb.rct): return 
+        pg.display.update()
+        clock.tick(1000)
 
 if __name__ == "__main__":
     pg.init()
